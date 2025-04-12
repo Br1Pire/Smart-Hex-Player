6f25400f2cf78b3edc11c9ef67b94a76d11c1308
Bruno Pire <br1.pire@gmail.com>
2025-04-11 22:28:23 -0400

    fixed an error in weight calculation

--- player.py
+++ player.py
@@ -17,7 +17,6 @@ class SmartPlayer(Player):
 
     def play(self, board: HexBoard) -> tuple:
         possible_moves = board.get_possible_moves()
-        weights = self.generate_weight_matrix(board.size)
 
         best_score = float('-inf')
         best_move = []
@@ -26,7 +25,7 @@ class SmartPlayer(Player):
             aux_board = board.clone()
             aux_board.place_piece(move[0], move[1], self.player_id)
 
-            score = self.minimax(aux_board, weights, 3, float('-inf'), float('inf'), False)
+            score = self.minimax(aux_board, 3, float('-inf'), float('inf'), False)
 
             if score > best_score:
                 best_score = score
@@ -36,9 +35,9 @@ class SmartPlayer(Player):
 
         return random.choice(best_move)
 
-    def minimax(self, board, weights, depth, alpha, beta, maximizing):
+    def minimax(self, board, depth, alpha, beta, maximizing):
         if depth == 0 or board.check_connection(self.player_id) :
-            return self.evaluate_board(board, weights)
+            return self.evaluate_board(board)
 
         possible_moves = board.get_possible_moves()
         current_player = self.player_id if maximizing else 2
@@ -48,7 +47,7 @@ class SmartPlayer(Player):
             for move in possible_moves:
                 aux = board.clone()
                 aux.place_piece(move[0], move[1], current_player)
-                eval = self.minimax(aux, weights, depth - 1, alpha, beta, False)
+                eval = self.minimax(aux, depth - 1, alpha, beta, False)
                 max_eval = max(max_eval, eval)
                 alpha = max(alpha, eval)
                 if beta <= alpha:
@@ -59,14 +58,14 @@ class SmartPlayer(Player):
             for move in possible_moves:
                 aux = board.clone()
                 aux.place_piece(move[0], move[1], current_player)
-                eval = self.minimax(aux, weights, depth - 1, alpha, beta, True)
+                eval = self.minimax(aux, depth - 1, alpha, beta, True)
                 min_eval = min(min_eval, eval)
                 beta = min(beta, eval)
                 if beta <= alpha:
                     break
             return min_eval   
         
-    def evaluate_board(self, board, weights):
+    def evaluate_board(self, board):
         
         n = board.size  
         turns_amount = len(board.player_positions[1]) + len(board.player_positions[2])
@@ -74,14 +73,14 @@ class SmartPlayer(Player):
         percent_turns = n * n * 0.60
 
         if turns_amount < min_turns_to_win:
-            return self.evaluate_early_game(board, weights)
+            return self.evaluate_early_game(board)
         elif turns_amount < percent_turns:
-            return self.evaluate_mid_game(board, weights)
+            return self.evaluate_mid_game(board)
         else:
             return self.evaluate_end_game(board)
         
-    def evaluate_early_game(self, board, weights):
-        #weights = self.generate_weight_matrix(board.size)
+    def evaluate_early_game(self, board):
+        weights = self.generate_weight_matrix(board.size)
 
         check_list = board.player_positions[2]
 
@@ -92,19 +91,25 @@ class SmartPlayer(Player):
                     weights[nx][ny] += 1
 
         score = 0
-        for i in range(board.size):
-            for j in range(board.size):
-                if board.board[i][j] == self.player_id:
-                    score += weights[i][j]
-                elif board.board[i][j] == 2:
-                    score -= weights[i][j]
+        # for i in range(board.size):
+        #     for j in range(board.size):
+        #         if board.board[i][j] == self.player_id:
+        #             score += weights[i][j]
+        #         elif board.board[i][j] == 2:
+        #             score -= weights[i][j]
+
+        for i in board.player_positions[self.player_id]:
+            score += weights[i[0]][i[1]]
+        for i in board.player_positions[2]:
+            score -= weights[i[0]][i[1]]
+            
         return score
     
     def generate_weight_matrix(self, n):
         return [[n - abs(i - n//2) - abs(j - n//2) for j in range(n)] for i in range(n)]
     
-    def evaluate_mid_game(self, board, weights):
-        board_eval = self.evaluate_early_game(board, weights)
+    def evaluate_mid_game(self, board):
+        board_eval = self.evaluate_early_game(board)
         own_nodes_to_connect = self.estimate_remaining_nodes_to_connect(board, self.player_id)
         opp_nodes_to_connect = self.estimate_remaining_nodes_to_connect(board, 2)
 
